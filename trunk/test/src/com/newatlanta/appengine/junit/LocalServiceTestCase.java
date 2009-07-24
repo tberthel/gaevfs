c5ddff1360af37049b5c67af79ce24811d74f306
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-07-24 19:05:36 +0000

    junit support

---
+++ trunk/test/src/com/newatlanta/appengine/junit/LocalServiceTestCase.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2009 New Atlanta Communications, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.newatlanta.appengine.junit;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import com.google.appengine.tools.development.ApiProxyLocalImpl;
+import com.google.apphosting.api.ApiProxy;
+
+/**
+ * http://code.google.com/appengine/docs/java/howto/unittesting.html
+ */
+public abstract class LocalServiceTestCase extends TestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        ApiProxy.setEnvironmentForCurrentThread(new TestEnvironment());
+        ApiProxy.setDelegate(new ApiProxyLocalImpl(new File(".")) {
+        });
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        // not strictly necessary to null these out but there's no harm either
+        ApiProxy.setDelegate(null);
+        ApiProxy.setEnvironmentForCurrentThread(null);
+        super.tearDown();
+    }
+}
+
+class TestEnvironment implements ApiProxy.Environment {
+    public String getAppId() {
+        return "Unit Tests";
+    }
+
+    public String getVersionId() {
+        return "1.0";
+    }
+
+    public String getRequestNamespace() {
+        return "gmail.com";
+    }
+
+    public String getAuthDomain() {
+        return "gmail.com";
+    }
+
+    public boolean isLoggedIn() {
+        return false;
+    }
+
+    public String getEmail() {
+        return "";
+    }
+
+    public boolean isAdmin() {
+        return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map getAttributes() {
+        return new HashMap();
+    }
+}
