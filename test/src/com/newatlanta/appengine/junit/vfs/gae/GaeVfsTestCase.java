90628478cd9fed72ccc1579634663efb64486135
vbonfanti <vbonfanti@gmail.com>
2009-07-31 19:15:55 +0000

    junit tests

--- test/src/com/newatlanta/appengine/junit/vfs/gae/GaeVfsTestCase.java
+++ test/src/com/newatlanta/appengine/junit/vfs/gae/GaeVfsTestCase.java
@@ -17,6 +17,8 @@ package com.newatlanta.appengine.junit.vfs.gae;
 
 import java.io.File;
 
+import org.apache.commons.vfs.FileObject;
+
 import com.newatlanta.appengine.junit.LocalDatastoreTestCase;
 import com.newatlanta.commons.vfs.provider.gae.GaeVFS;
 
@@ -35,7 +37,38 @@ public abstract class GaeVfsTestCase extends LocalDatastoreTestCase {
     
     @Override
     public void tearDown() throws Exception {
-        GaeVFS.clearFilesCache();
         super.tearDown();
+    }
+    
+    public static void assertEquals( File file, FileObject fileObject ) throws Exception {
+    	assertEqualPaths( file, fileObject );
+		assertEquals( file.canRead(), fileObject.isReadable() );
+		assertEquals( file.canWrite(), fileObject.isWriteable() );
+		assertEquals( file.exists(), fileObject.exists() );
+		if ( file.getParentFile() == null ) {
+			assertNull( fileObject.getParent() );
+		} else {
+			assertEqualPaths( file.getParentFile(), fileObject.getParent() );
+		}
+		assertEquals( file.isDirectory(), fileObject.getType().hasChildren() );
+		assertEquals( file.isFile(), fileObject.getType().hasContent() );
+		assertEquals( file.isHidden(), fileObject.isHidden() );
+		if ( file.isFile() ) {
+			assertEquals( file.length(), fileObject.getContent().getSize() );
+		}
+		if ( file.isDirectory() ) { // same children
+			File[] childFiles = file.listFiles();
+			FileObject[] childObjects = fileObject.getChildren();
+			assertEquals( childFiles.length, childObjects.length );
+			for ( int i = 0; i < childFiles.length; i++ ) {
+				assertEqualPaths( childFiles[ i ], childObjects[ i ] );
+			}
+		}
+    }
+    
+    public static void assertEqualPaths( File file, FileObject fileObject ) throws Exception {
+    	String fileObjectPath = new File( fileObject.getName().getPath() ).getAbsolutePath();
+    	assertEquals( file.getAbsolutePath(), fileObjectPath ); 
+    	assertEquals( file.getName(), fileObject.getName().getBaseName() );
     }
 }
