8a1bd5e1901599c7813d645c692fc5e24a4cd07a
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-08-17 21:29:40 +0000

    add license and comments

--- test/src/com/newatlanta/appengine/junit/locks/LockingThread.java
+++ test/src/com/newatlanta/appengine/junit/locks/LockingThread.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 New Atlanta Communications, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.newatlanta.appengine.junit.locks;
 
 import java.util.concurrent.locks.Lock;
@@ -6,6 +21,12 @@ import com.google.apphosting.api.ApiProxy;
 import com.google.apphosting.api.ApiProxy.Delegate;
 import com.newatlanta.appengine.junit.TestEnvironment;
 
+/**
+ * Acquires the specified lock, then sleeps for the specified time or until
+ * interrupted.
+ * 
+ * @author <a href="mailto:vbonfanti@gmail.com">Vince Bonfanti</a>
+ */
 public class LockingThread extends Thread {
     
     private Lock lock;
@@ -32,6 +53,7 @@ public class LockingThread extends Thread {
         this.lock = lock;
     }
 
+    @Override
     public void run() {
         ApiProxy.setEnvironmentForCurrentThread( new TestEnvironment() );
         ApiProxy.setDelegate( delegate );
