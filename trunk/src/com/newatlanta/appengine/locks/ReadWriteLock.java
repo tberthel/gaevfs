b721672bef4e965754535f83cb25282cc9185ecd
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-08-17 00:34:07 +0000

    fix formatting (use spaces instead of tabs)

--- trunk/src/com/newatlanta/appengine/locks/ReadWriteLock.java
+++ trunk/src/com/newatlanta/appengine/locks/ReadWriteLock.java
@@ -27,141 +27,141 @@ import java.util.concurrent.locks.Lock;
  */
 public class ReadWriteLock implements java.util.concurrent.locks.ReadWriteLock {
 
-	private ReadLock readLock;
-	private WriteLock writeLock;
-	
-	public ReadWriteLock( String lockName ) {
-		readLock = new ReadLock( lockName + ".readLock" );
-		writeLock = new WriteLock( lockName + ".writeLock" );
-	}
-	
-	public Lock readLock() {
-		return readLock;
-	}
+    private ReadLock readLock;
+    private WriteLock writeLock;
 
-	public Lock writeLock() {
-		return writeLock;
-	}
-	
-	/**
-	 * Implements a SharedLock with an associated exclusive write lock.
-	 */
-	private class ReadLock extends SharedLock {
+    public ReadWriteLock( String lockName ) {
+        readLock = new ReadLock( lockName + ".readLock" );
+        writeLock = new WriteLock( lockName + ".writeLock" );
+    }
 
-		private ReadLock( String lockName ) {
-			super( lockName );
-		}
-		
-		/**
-		 * Acquires a shared read lock. First acquires the exclusive write lock
-		 * to make sure no other thread has it, then acquires the read lock (which
-		 * never fails). Releases the write lock before returning.
-		 * 
-		 * @return <code>true</code> if the read lock was acquired successfully
-		 *         <code>false</code> if unable to acquire the read lock due to
-		 *         the inability to acquire the associated write lock
-		 */
-		@Override
-		public boolean tryLock() {
-			if ( writeLock.tryLock() ) {
-				try {
-					return super.tryLock();
-				} finally {
-					writeLock.unlock();
-				}
-			}
-			return false; // couldn't acquire writeLock
-		}
-	}
-	
-	/**
-	 * Implements an ExclusiveLock with an associated shared read lock.
-	 */
-	private class WriteLock extends ExclusiveLock {
+    public Lock readLock() {
+        return readLock;
+    }
 
-		private WriteLock( String lockName ) {
-			super( lockName );
-		}
-		
-		/**
-		 * Acquires the lock. If the lock is not available then the current thread
-		 * becomes disabled for thread scheduling purposes and lies dormant until
-		 * the lock has been acquired.
-		 * 
-		 * Note that GAE request threads timeout after 30 seconds, so this won't
-		 * run forever.
-		 */
-		@Override
-		public void lock() {
-			super.lock(); // acquire the exclusive lock
-			long sleepTime = 1;
-			while ( readLock.isLocked() ) { // make sure no readers
-				try {
-					// sleep twice as long after each iteration
-					Thread.sleep( Math.min( MAX_SLEEP_TIME, sleepTime <<= 1 ) );
-				} catch ( InterruptedException ignore ) {
-				}
-			}
-		}
-		
-		/**
-		 * Acquires the lock unless the current thread is interrupted.
-		 * 
-		 * Note that GAE request threads timeout after 30 seconds, so this won't
-		 * run forever.
-		 */
-		@Override
-		public void lockInterruptibly() throws InterruptedException {
-			super.lockInterruptibly(); // acquire the exclusive lock
-			long sleepTime = 1;
-			while ( readLock.isLocked() ) { // make sure no readers
-				// sleep twice as long after each iteration
-				Thread.sleep( Math.min( MAX_SLEEP_TIME, sleepTime <<= 1 ) );
-			}
-		}
-		
-		/**
-		 * Acquires the lock if it is free within the given waiting time and the
-		 * current thread has not been interrupted.
-		 * 
-		 * Note that GAE request threads timeout after 30 seconds.
-		 */
-		@Override
-		public boolean tryLock( long time, TimeUnit unit ) {
-			long startTime = System.currentTimeMillis();
-			super.tryLock( time, unit ); // acquire the exclusive lock
-			long waitTime = Math.min( 0, unit.toMillis( time ) );
-			long sleepTime = 1;
-			try {
-				do {
-					if ( !readLock.isLocked() ) { // make sure no readers
-						return true;
-					}
-					// sleep twice as long after each iteration
-					Thread.sleep( Math.min( MAX_SLEEP_TIME, sleepTime <<= 1 ) );
-				} while ( ( System.currentTimeMillis() - startTime ) < waitTime );
-			} catch ( InterruptedException ignore ) {
-			}
-			return false;
-		}
-		
-		/**
-		 * Acquires an exclusive write lock. First acquires the write lock to make
-		 * sure no other thread has it, then makes sure no readers already own the
-		 * lock.
-		 * 
-		 * @return <code>true</code> if the write lock was acquired successfully
-		 *         <code>false</code> if unable to acquire the write lock
-		 */
-		@Override
-		public boolean tryLock() {
-			if ( super.tryLock() ) { // acquire the exclusive write lock
-				if ( !readLock.isLocked() ) { // make sure no readers
-					return true;
-				}
-				super.unlock();
-			}
-			return false;
-		}
-	}
+    public Lock writeLock() {
+        return writeLock;
+    }
+
+    /**
+     * Implements a SharedLock with an associated exclusive write lock.
+     */
+    private class ReadLock extends SharedLock {
+
+        private ReadLock( String lockName ) {
+            super( lockName );
+        }
+
+        /**
+         * Acquires a shared read lock. First acquires the exclusive write lock
+         * to make sure no other thread has it, then acquires the read lock (which
+         * never fails). Releases the write lock before returning.
+         * 
+         * @return <code>true</code> if the read lock was acquired successfully
+         *         <code>false</code> if unable to acquire the read lock due to
+         *         the inability to acquire the associated write lock
+         */
+        @Override
+        public boolean tryLock() {
+            if ( writeLock.tryLock() ) {
+                try {
+                    return super.tryLock();
+                } finally {
+                    writeLock.unlock();
+                }
+            }
+            return false; // couldn't acquire writeLock
+        }
+    }
+
+    /**
+     * Implements an ExclusiveLock with an associated shared read lock.
+     */
+    private class WriteLock extends ExclusiveLock {
+
+        private WriteLock( String lockName ) {
+            super( lockName );
+        }
+
+        /**
+         * Acquires the lock. If the lock is not available then the current thread
+         * becomes disabled for thread scheduling purposes and lies dormant until
+         * the lock has been acquired.
+         * 
+         * Note that GAE request threads timeout after 30 seconds, so this won't
+         * run forever.
+         */
+        @Override
+        public void lock() {
+            super.lock(); // acquire the exclusive lock
+            long sleepTime = 1;
+            while ( readLock.isLocked() ) { // make sure no readers
+                try {
+                    // sleep twice as long after each iteration
+                    Thread.sleep( Math.min( MAX_SLEEP_TIME, sleepTime <<= 1 ) );
+                } catch ( InterruptedException ignore ) {
+                }
+            }
+        }
+
+        /**
+         * Acquires the lock unless the current thread is interrupted.
+         * 
+         * Note that GAE request threads timeout after 30 seconds, so this won't
+         * run forever.
+         */
+        @Override
+        public void lockInterruptibly() throws InterruptedException {
+            super.lockInterruptibly(); // acquire the exclusive lock
+            long sleepTime = 1;
+            while ( readLock.isLocked() ) { // make sure no readers
+                // sleep twice as long after each iteration
+                Thread.sleep( Math.min( MAX_SLEEP_TIME, sleepTime <<= 1 ) );
+            }
+        }
+
+        /**
+         * Acquires the lock if it is free within the given waiting time and the
+         * current thread has not been interrupted.
+         * 
+         * Note that GAE request threads timeout after 30 seconds.
+         */
+        @Override
+        public boolean tryLock( long time, TimeUnit unit ) {
+            long startTime = System.currentTimeMillis();
+            super.tryLock( time, unit ); // acquire the exclusive lock
+            long waitTime = Math.min( 0, unit.toMillis( time ) );
+            long sleepTime = 1;
+            try {
+                do {
+                    if ( !readLock.isLocked() ) { // make sure no readers
+                        return true;
+                    }
+                    // sleep twice as long after each iteration
+                    Thread.sleep( Math.min( MAX_SLEEP_TIME, sleepTime <<= 1 ) );
+                } while ( ( System.currentTimeMillis() - startTime ) < waitTime );
+            } catch ( InterruptedException ignore ) {
+            }
+            return false;
+        }
+
+        /**
+         * Acquires an exclusive write lock. First acquires the write lock to make
+         * sure no other thread has it, then makes sure no readers already own the
+         * lock.
+         * 
+         * @return <code>true</code> if the write lock was acquired successfully
+         *         <code>false</code> if unable to acquire the write lock
+         */
+        @Override
+        public boolean tryLock() {
+            if ( super.tryLock() ) { // acquire the exclusive write lock
+                if ( !readLock.isLocked() ) { // make sure no readers
+                    return true;
+                }
+                super.unlock();
+            }
+            return false;
+        }
+    }
 }
