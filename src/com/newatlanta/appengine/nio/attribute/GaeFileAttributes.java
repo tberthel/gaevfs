5d02acd9de66d3b55a7cdc0c3a35e12895f6525f
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-08-28 17:36:58 +0000

    add support for file attributes

---
+++ src/com/newatlanta/appengine/nio/attribute/GaeFileAttributes.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2009 New Atlanta Communications, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.newatlanta.appengine.nio.attribute;
+
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemException;
+
+import com.newatlanta.commons.vfs.provider.gae.GaeFileObject;
+import com.newatlanta.nio.file.attribute.BasicFileAttributes;
+import com.newatlanta.nio.file.attribute.FileTime;
+
+public class GaeFileAttributes implements BasicFileAttributes {
+
+    private FileObject fileObject;
+    
+    GaeFileAttributes( FileObject fileObject ) {
+        this.fileObject = fileObject;
+    }
+    
+    public FileTime creationTime() {
+        return null; // not supported
+    }
+
+    /**
+     * Used by Files.walkFileTree() to detect cycles created by symbolic links.
+     */
+    public Object fileKey() {
+        return null; // not supported
+    }
+
+    public boolean isDirectory() {
+        try {
+            return fileObject.getType().hasChildren();
+        } catch ( FileSystemException e ) {
+            return false;
+        }
+    }
+
+    public boolean isOther() {
+        return false; // not supported
+    }
+
+    public boolean isRegularFile() {
+        try {
+            return fileObject.getType().hasContent();
+        } catch ( FileSystemException e ) {
+            return false;
+        }
+    }
+
+    public boolean isSymbolicLink() {
+        return false; // not supported
+    }
+
+    public FileTime lastAccessTime() {
+        return null; // not supported
+    }
+
+    public FileTime lastModifiedTime() {
+        try {
+            return FileTime.fromMillis( fileObject.getContent().getLastModifiedTime() );
+        } catch ( FileSystemException e ) {
+            return null;
+        }
+    }
+
+    public long size() {
+        try {
+            return fileObject.getContent().getSize();
+        } catch ( FileSystemException e ) {
+            return 0;
+        }
+    }
+    
+    public int getBlockSize() {
+        try {
+            if ( fileObject instanceof GaeFileObject ) {
+                ((GaeFileObject)fileObject).getBlockSize();
+            }
+        } catch ( FileSystemException e ) {
+        }
+        return 0;
+    }
+}
