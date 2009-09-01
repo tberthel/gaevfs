1e60d0373c7dd5966328ff261d805a088fff119d
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-09-01 21:11:11 +0000

    experimental implementation based on GaeRandomAccessContent; never tested, being replaced

--- trunk/src/com/newatlanta/appengine/nio/channels/GaeFileChannel.java
+++ trunk/src/com/newatlanta/appengine/nio/channels/GaeFileChannel.java
@@ -17,109 +17,301 @@ package com.newatlanta.appengine.nio.channels;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-//import java.nio.MappedByteBuffer;
-//import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
 import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.SeekableByteChannel;
 import java.nio.channels.WritableByteChannel;
 
-public class GaeFileChannel /*extends FileChannel*/ implements SeekableByteChannel {
+import org.apache.commons.vfs.FileSystemException;
 
-    public GaeFileChannel() {
-        // TODO Auto-generated constructor stub
+import com.google.appengine.api.datastore.Blob;
+import com.google.appengine.api.datastore.Entity;
+import com.newatlanta.commons.vfs.provider.gae.GaeFileObject;
+import com.newatlanta.nio.channels.FileChannel;
+import com.newatlanta.nio.channels.FileLock;
+
+public class GaeFileChannel extends FileChannel {
+    
+    private static final String CONTENT_BLOB = "content-blob"; // property key
+    
+    private GaeFileObject fileObject; // parent file
+    
+    private Entity block;   // the current block
+    private int index;      // index of the current block
+    private boolean dirty;  // current block needs to be written?
+    
+    private long position; // absolute position within the file
+    
+    private byte[] buffer;  // current block buffer
+    private int offset;     // relative position of filePointer within buffer
+    private int blockSize;
+
+    public GaeFileChannel( GaeFileObject gfo, int _blockSize ) {
+        fileObject = gfo;
+        blockSize = _blockSize;
     }
-
-    public void force( boolean arg0 ) throws IOException {
+    
+    @Override
+    public void force( boolean metaData ) throws IOException {
         // TODO Auto-generated method stub
-
     }
 
-    public FileLock lock( long arg0, long arg1, boolean arg2 ) throws IOException {
+    @Override
+    public FileLock lock( long position, long size, boolean shared ) throws IOException {
         // TODO Auto-generated method stub
         return null;
     }
 
-//    public MappedByteBuffer map( MapMode arg0, long arg1, long arg2 ) throws IOException {
-//        // TODO Auto-generated method stub
-//        return null;
-//    }
-
+    @Override
     public long position() throws IOException {
+        return position;
+    }
+
+    @Override
+    public synchronized GaeFileChannel position( long newPosition ) throws IOException {
+        if ( position == newPosition ) {
+            return this;
+        }
+        if ( newPosition < 0 ) {
+            throw new IllegalArgumentException( "invalid position: " + newPosition );
+        }
+        int newIndex = calcBlockIndex( newPosition );
+        if ( newIndex != index ) {
+            close();
+            index = newIndex;
+        }
+        position = newPosition;
+        offset = calcBlockOffset( position );
+        return this;
+    }
+    
+    /**
+     * Given an absolute position within the file, calculate the block index.
+     */
+    private int calcBlockIndex( long i ) {
+        return (int)( i / blockSize );
+    }
+    
+    /**
+     * Given an absolute position within the file, calculate the offset within
+     * the current block.
+     */
+    private int calcBlockOffset( long i ) {
+        return (int)( i - ( index * blockSize ) );
+    }
+
+    @Override
+    public long read( ByteBuffer[] dsts, int offset, int length ) throws IOException {
         // TODO Auto-generated method stub
         return 0;
     }
 
-    public SeekableByteChannel position( long arg0 ) throws IOException {
+    @Override
+    public int read( ByteBuffer dst, long position ) throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int read( ByteBuffer dst ) throws IOException {
+        int len = dst.remaining();
+        if ( len == 0 ) {
+            return 0;
+        }
+        long fileLen = size();
+        if ( position >= fileLen ) {
+            return -1;
+        }
+        if ( position + len > fileLen ) {
+            len = (int)( fileLen - position );
+        }
+        if ( len <= 0 ) {
+            return 0;
+        }
+        return internalRead( dst, len );
+    }
+    
+    private synchronized int internalRead( ByteBuffer dst, int len ) throws IOException {
+        // len is always less than or equal to the file length, so we're
+        // always going to read exactly len number of bytes
+        initBuffer();
+        if ( calcBlockIndex( position + len ) == index ) { // within current block
+            dst.put( buffer, offset, len );
+            position( position + len );
+            return len; // recursive reads always end here
+        } else {
+            // read to the end of the current buffer
+            int bytesAvailable = buffer.length - offset;
+            dst.put( buffer, offset, bytesAvailable );
+
+            // move file pointer to beginning of next buffer
+            position( position + bytesAvailable );
+
+            // recursively read the rest of the input
+            internalRead( dst, len - bytesAvailable );
+            return len;
+        }
+    }
+
+    @Override
+    public long size()throws IOException {
+        return fileObject.getContent().getSize();
+    }
+
+    @Override
+    public long transferFrom( ReadableByteChannel src, long position, long count ) throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public long transferTo( long position, long count, WritableByteChannel target ) throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public GaeFileChannel truncate( long size ) throws IOException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public int read( ByteBuffer arg0 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public int read( ByteBuffer arg0, long arg1 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public long read( ByteBuffer[] arg0, int arg1, int arg2 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public long size() throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public long transferFrom( ReadableByteChannel arg0, long arg1, long arg2 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public long transferTo( long arg0, long arg1, WritableByteChannel arg2 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public SeekableByteChannel truncate( long arg0 ) throws IOException {
+    @Override
+    public FileLock tryLock( long position, long size, boolean shared ) throws IOException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public FileLock tryLock( long arg0, long arg1, boolean arg2 ) throws IOException {
-        // TODO Auto-generated method stub
-        return null;
+    @Override
+    public synchronized long write( ByteBuffer[] srcs, int offset, int length ) throws IOException {
+        if ( ( offset < 0 ) || ( offset > srcs.length ) ||
+                ( length < 0 ) || ( length > ( srcs.length - offset ) ) ) {
+            throw new IllegalArgumentException();
+        }
+        int bytesWritten = 0;
+        for ( int i = offset; i < ( offset + length ); i++ ) {
+            bytesWritten += write( srcs[ i ] );
+        }
+        return bytesWritten;
     }
 
-    public int write( ByteBuffer arg0 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
+    @Override
+    public synchronized int write( ByteBuffer src, long writePos ) throws IOException {
+        long origPosition = position;
+        try {
+            return position( writePos ).write( src );
+        } finally {
+            position( origPosition );
+        }
+    }
+    
+    @Override
+    public synchronized int write( ByteBuffer src ) throws IOException {
+        int len = src.remaining();
+        if ( len > 0 ) {
+            internalWrite( src, len );
+        }
+        return len;
+    }
+    
+    private synchronized void internalWrite( ByteBuffer src, int len ) throws IOException {
+        initBuffer();
+        if ( calcBlockIndex( position + len ) == index ) { // within current block
+            writeBuffer( src, len );
+            moveFilePointer( position + len );
+        } else {
+            // fill the current buffer
+            int bytesAvailable = blockSize - offset;
+            writeBuffer( src, bytesAvailable );
+            moveFilePointer( position + bytesAvailable );
+
+            // recursively write the rest of the output
+            internalWrite( src, len - bytesAvailable );
+        }
+    }
+    
+    private synchronized void writeBuffer( ByteBuffer src, int len ) throws FileSystemException {
+        if ( ( offset + len ) > buffer.length ) {
+            extendBuffer( offset + len );
+        }
+        src.get( buffer, offset, len );
+        dirty = true;
+    }
+    
+    /**
+     * The preferred extended buffer size is twice the current size, but it must be:
+     *      - at least as large as len
+     *      - at least as large as the minimum buffer size
+     *      - no larger than the file block size
+     */
+    private synchronized void extendBuffer( int len ) throws FileSystemException {
+        byte[] tempBuf = buffer;
+        // twice the current size, but at least as large as len
+        int newSize = Math.max( buffer.length << 1, len );
+        // at least as large as the minimum size
+        newSize = Math.max( newSize, getMinBufferSize() );
+        // no larger than the block size
+        buffer = new byte[ Math.min( newSize, blockSize ) ];
+        System.arraycopy( tempBuf, 0, buffer, 0, tempBuf.length );
     }
 
-    public int write( ByteBuffer arg0, long arg1 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
+    private synchronized void moveFilePointer( long newPos ) throws IOException {
+        fileObject.updateContentSize( newPos );
+        position( newPos );
+    }
+    
+    private synchronized void initBuffer() throws FileSystemException {
+        if ( buffer != null ) {
+            return;
+        }
+        if ( block == null ) {
+            block = fileObject.getBlock( index );
+            dirty = false;
+        }
+        Blob contentBlob = (Blob)block.getProperty( CONTENT_BLOB );
+        buffer = ( contentBlob != null ? contentBlob.getBytes()
+                                       : new byte[ getMinBufferSize() ] );
+    }
+    
+    private int getMinBufferSize() throws FileSystemException {
+        if ( blockSize <= ( 1024 * 8 ) ) {
+            return blockSize;
+        } else if ( blockSize <= ( 1024 * 32 ) ) {
+            return 1024 * 8;
+        } else if ( blockSize >= ( 1024 * 256 ) ) {
+            return 1024 * 64;
+        } else {
+            return blockSize >> 2; // one-fourth block size
+        }
     }
 
-    public long write( ByteBuffer[] arg0, int arg1, int arg2 ) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    protected void implCloseChannel() throws IOException {
-        // TODO Auto-generated method stub
-    }
-
-    public void close() throws IOException {
-        // TODO Auto-generated method stub   
-    }
-
-    public boolean isOpen() {
-        // TODO Auto-generated method stub
-        return false;
+    @Override
+    protected synchronized void implCloseChannel() throws IOException {
+        if ( dirty && ( block != null ) && ( buffer != null ) ) {
+            boolean setBlobProperty = true;
+            long fileLen = size();
+            // if this is the last block for the file, and the buffer is less than
+            // half full, only write out the actual number of bytes
+            if ( calcBlockIndex( fileLen ) == index ) {
+                // the EOF offset could be larger than buffer.length if setLength()
+                // is used to set the file length larger than buffer.length, but no
+                // bytes get written past buffer.length
+                int eofoffset = calcBlockOffset( fileLen );
+                if ( eofoffset < ( buffer.length >> 1 ) ) { // less than half full
+                    byte[] outbuf = new byte[ eofoffset ];
+                    System.arraycopy( buffer, 0, outbuf, 0, outbuf.length );
+                    block.setProperty( CONTENT_BLOB, new Blob( outbuf ) );
+                    setBlobProperty = false;
+                }
+            }
+            if ( setBlobProperty ) {
+                block.setProperty( CONTENT_BLOB, new Blob( buffer ) );
+            }
+            fileObject.putBlock( block );
+            dirty = false;
+        }
+        block = null;
+        buffer = null;
+        index = 0;
+        position = 0;
+        offset = 0;
     }
 }
