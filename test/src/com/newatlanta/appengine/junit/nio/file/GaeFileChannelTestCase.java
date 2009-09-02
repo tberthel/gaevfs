a18ec137047c2e33ce789b13f2890b0ae4d52131
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-09-02 20:45:27 +0000

    newByteChannel() implementation and test cases (and some miscellaneous other changes)

---
+++ test/src/com/newatlanta/appengine/junit/nio/file/GaeFileChannelTestCase.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2009 New Atlanta Communications, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.newatlanta.appengine.junit.nio.file;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GaeFileChannelTestCase {
+
+    @Test
+    public void testReadByteBuffer() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testReadByteBufferArrayIntInt() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testWriteByteBuffer() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testWriteByteBufferArrayIntInt() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testPosition() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testSize() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testForce() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testTransferTo() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testTransferFrom() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testReadByteBufferLong() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testWriteByteBufferLong() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testLockLongLongBoolean() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testTryLockLongLongBoolean() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testPositionLong() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testTruncateLong() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testOpenPathSetOfQextendsOpenOptionFileAttributeOfQArray() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testOpenPathOpenOptionArray() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testClose() {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testIsOpen() {
+        fail( "Not yet implemented" );
+    }
+
+}
