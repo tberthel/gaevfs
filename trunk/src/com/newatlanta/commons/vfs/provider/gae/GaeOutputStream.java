eb96b846fa9bf0b01c909954a1b7c7c65b0fef0b
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-09-04 20:11:04 +0000

    bypass Commons VFS automatic buffering of output stream

---
+++ trunk/src/com/newatlanta/commons/vfs/provider/gae/GaeOutputStream.java
@@ -0,0 +1,42 @@
+package com.newatlanta.commons.vfs.provider.gae;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.vfs.RandomAccessContent;
+
+public class GaeOutputStream extends OutputStream {
+    
+    private RandomAccessContent out;
+    
+    public GaeOutputStream( RandomAccessContent rac, boolean append ) throws IOException {
+        out = rac;
+        if ( append ) {
+            rac.seek( rac.length() );
+        }
+    }
+
+    @Override
+    public void write( int b ) throws IOException {
+        out.write( b );
+    }
+    
+    @Override
+    public void write( byte[] b ) throws IOException {
+        out.write( b );
+    }
+    
+    @Override
+    public void write( byte[] b, int off, int len ) throws IOException {
+        out.write( b, off, len );
+    }
+    
+    @Override
+    public void flush() throws IOException {
+    }
+    
+    @Override
+    public void close() throws IOException {
+        out.close();
+    }
+}
