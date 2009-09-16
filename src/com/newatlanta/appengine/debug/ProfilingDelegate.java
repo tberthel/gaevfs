c27d0470af47f9e5eee72252aba186207da1ebbe
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-09-16 20:28:13 +0000

    for debugging

---
+++ src/com/newatlanta/appengine/debug/ProfilingDelegate.java
@@ -0,0 +1,32 @@
+package com.newatlanta.appengine.debug;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import com.google.apphosting.api.ApiProxy.Delegate;
+import com.google.apphosting.api.ApiProxy.Environment;
+import com.google.apphosting.api.ApiProxy.LogRecord;
+
+public class ProfilingDelegate implements Delegate<Environment> {
+    
+    private static final Logger log = Logger.getLogger( ProfilingDelegate.class.getName() );
+    
+    private Delegate<Environment> parent;
+    
+    public ProfilingDelegate( Delegate<Environment> parent ) {
+      this.parent = parent;
+    }
+    
+    public byte[] makeSyncCall( Environment env, String pkg, String method, byte[] request ) {
+      long start = System.nanoTime();
+      byte[] result = parent.makeSyncCall( env, pkg, method, request );
+      log.info( pkg + "." + method + ": " +
+              TimeUnit.MILLISECONDS.convert( System.nanoTime() - start, TimeUnit.NANOSECONDS ) + "ms" );
+      return result;
+    }
+    
+    public void log(Environment env, LogRecord logRec) {
+        parent.log(env, logRec);
+    }
+
+}
