ef5ef857c77c815e2e43e54a659cf25b702cf00f
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-09-17 22:14:20 +0000

    implement lock() and tryLock() for entire files (regions not yet supported)

--- trunk/src/com/newatlanta/appengine/nio/channels/GaeFileLock.java
+++ trunk/src/com/newatlanta/appengine/nio/channels/GaeFileLock.java
@@ -16,19 +16,56 @@
 package com.newatlanta.appengine.nio.channels;
 
 import java.io.IOException;
+import java.nio.channels.OverlappingFileLockException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.locks.Lock;
 
 import com.newatlanta.nio.channels.FileChannel;
 import com.newatlanta.nio.channels.FileLock;
 
 public class GaeFileLock extends FileLock {
+    
+    private static Map<String, List<GaeFileLock>> fileLocks =
+        Collections.synchronizedMap( new HashMap<String, List<GaeFileLock>>() );
 
     private Lock lock;
+    private String name;
     
-    public GaeFileLock( FileChannel fileChannel, long position, long size,
-                          boolean shared, Lock lock ) {
-        super( fileChannel, position, size, shared );
+    GaeFileLock( GaeFileChannel fileChannel, long position, long size,
+                    boolean shared ) throws IOException
+    {
+        super( fileChannel, position, size, shared ); // validates position and size
+        fileChannel.checkOpen();
+        name = fileChannel.getLockName();
+        
+        // multiple locks--whether exclusive or shared--must not overlap
+        synchronized ( fileLocks ) {
+            List<GaeFileLock> lockList = fileLocks.get( name );
+            if ( lockList != null ) {
+                for ( GaeFileLock lock : lockList ) {
+                    if ( lock.overlaps( position, size ) ) {
+                        throw new OverlappingFileLockException();
+                    }
+                }
+            } else {
+                lockList = new ArrayList<GaeFileLock>();
+                fileLocks.put( name, lockList );
+            }
+            lockList.add( this );
+        }
+    }
+    
+    boolean isEntireFile() {
+        return ( ( position() == 0L ) && ( size() == Long.MAX_VALUE ) );
+    }
+    
+    FileLock acquired( Lock lock ) {
         this.lock = lock;
+        return this;
     }
 
     @Override
@@ -38,7 +75,38 @@ public class GaeFileLock extends FileLock {
 
     @Override
     public void release() throws IOException {
-        lock.unlock();
-        lock = null;
+        synchronized ( fileLocks ) {
+            List<GaeFileLock> lockList = fileLocks.get( name );
+            if ( lockList != null ) {
+                lockList.remove( this );
+                if ( lockList.isEmpty() ) {
+                    fileLocks.remove( name );
+                }
+            }
+        }
+        if ( lock != null ) {
+            lock.unlock();
+            lock = null;
+        }
+    }
+    
+    static void releaseAll( FileChannel fileChannel ) throws IOException {
+        synchronized( fileLocks ) {
+            for ( List<GaeFileLock> lockList : fileLocks.values() ) {
+                // use array to avoid ConcurrentModificationException
+                GaeFileLock[] locks = lockList.toArray( new GaeFileLock[ lockList.size() ] );
+                for ( GaeFileLock fileLock : locks ) {
+                    if ( ( fileChannel == null ) ||
+                            ( fileLock.acquiredBy() == fileChannel ) ) {
+                        fileLock.release();
+                    }
+                }
+            }
+        }
+    }
+    
+    public static void releaseAll() throws IOException {
+        releaseAll( null );
+        fileLocks.clear();
     }
 }
