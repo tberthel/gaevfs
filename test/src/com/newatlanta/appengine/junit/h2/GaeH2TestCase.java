99bbf349cdac5044eaab29d518d2e858fac310cb
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-10-23 18:25:20 +0000

    almost finished

--- test/src/com/newatlanta/appengine/junit/h2/GaeH2TestCase.java
+++ test/src/com/newatlanta/appengine/junit/h2/GaeH2TestCase.java
@@ -13,7 +13,6 @@ import org.h2.test.db.TestBigDb;
 import org.h2.test.db.TestBigResult;
 import org.h2.test.db.TestCases;
 import org.h2.test.db.TestCheckpoint;
-//import org.h2.test.db.TestCluster;
 import org.h2.test.db.TestCompatibility;
 import org.h2.test.db.TestCsv;
 import org.h2.test.db.TestDeadlock;
@@ -52,6 +51,59 @@ import org.h2.test.db.TestTransaction;
 import org.h2.test.db.TestTriggersConstraints;
 import org.h2.test.db.TestTwoPhaseCommit;
 import org.h2.test.db.TestView;
+import org.h2.test.jaqu.AliasMapTest;
+import org.h2.test.jaqu.SamplesTest;
+import org.h2.test.jaqu.UpdateTest;
+import org.h2.test.jdbc.TestBatchUpdates;
+import org.h2.test.jdbc.TestCallableStatement;
+import org.h2.test.jdbc.TestCancel;
+import org.h2.test.jdbc.TestDatabaseEventListener;
+import org.h2.test.jdbc.TestDriver;
+import org.h2.test.jdbc.TestManyJdbcObjects;
+import org.h2.test.jdbc.TestMetaData;
+import org.h2.test.jdbc.TestNativeSQL;
+import org.h2.test.jdbc.TestPreparedStatement;
+import org.h2.test.jdbc.TestResultSet;
+import org.h2.test.jdbc.TestStatement;
+import org.h2.test.jdbc.TestTransactionIsolation;
+import org.h2.test.jdbc.TestUpdatableResultSet;
+import org.h2.test.jdbc.TestZloty;
+import org.h2.test.jdbcx.TestConnectionPool;
+import org.h2.test.jdbcx.TestDataSource;
+import org.h2.test.jdbcx.TestXA;
+import org.h2.test.jdbcx.TestXASimple;
+import org.h2.test.mvcc.TestMvcc1;
+import org.h2.test.mvcc.TestMvcc2;
+import org.h2.test.mvcc.TestMvcc3;
+import org.h2.test.mvcc.TestMvccMultiThreaded;
+import org.h2.test.rowlock.TestRowLocks;
+import org.h2.test.server.TestNestedLoop;
+import org.h2.test.synth.TestBtreeIndex;
+import org.h2.test.synth.TestCrashAPI;
+import org.h2.test.synth.TestFuzzOptimizations;
+import org.h2.test.synth.TestMultiThreaded;
+import org.h2.test.synth.TestRandomSQL;
+import org.h2.test.unit.TestBitField;
+import org.h2.test.unit.TestCache;
+import org.h2.test.unit.TestClearReferences;
+import org.h2.test.unit.TestCompress;
+import org.h2.test.unit.TestDataPage;
+import org.h2.test.unit.TestDate;
+import org.h2.test.unit.TestDateIso8601;
+import org.h2.test.unit.TestFile;
+import org.h2.test.unit.TestIntArray;
+import org.h2.test.unit.TestIntIntHashMap;
+import org.h2.test.unit.TestMathUtils;
+import org.h2.test.unit.TestOverflow;
+import org.h2.test.unit.TestPattern;
+import org.h2.test.unit.TestRecovery;
+import org.h2.test.unit.TestScriptReader;
+import org.h2.test.unit.TestSecurity;
+import org.h2.test.unit.TestShell;
+import org.h2.test.unit.TestStringCache;
+import org.h2.test.unit.TestStringUtils;
+import org.h2.test.unit.TestValue;
+import org.h2.test.unit.TestValueHashMap;
 import org.junit.Test;
 
 import com.newatlanta.appengine.junit.vfs.gae.GaeVfsTestCase;
@@ -79,10 +131,14 @@ public class GaeH2TestCase extends GaeVfsTestCase {
     
     private void runTest( String className ) throws Exception {
         TestAll conf = new TestAll();
-        conf.jdk14 = false;
+        conf.pageStore = true;
         conf.googleAppEngine = true;
+        //conf.big = true;
+        //conf.traceTest = true;
         TestBase.createCaller( className ).init( conf ).test();
     }
+    
+    // db
     
     @Test
     public void testScriptSimple() throws Exception {
@@ -129,11 +185,6 @@ public class GaeH2TestCase extends GaeVfsTestCase {
         runTest( TestCheckpoint.class.getName() );
     }
     
-//    @Test
-//    public void testCluster() throws Exception {
-//        runTest( TestCluster.class.getName() );
-//    }
-    
     @Test
     public void testCompatibility() throws Exception {
         runTest( TestCompatibility.class.getName() );
@@ -150,13 +201,13 @@ public class GaeH2TestCase extends GaeVfsTestCase {
     }
     
     @Test
-    public void testEncryptedDb() throws Exception {
-        runTest( TestEncryptedDb.class.getName() );
+    public void testExclusive() throws Exception {
+        runTest( TestExclusive.class.getName() );
     }
     
     @Test
-    public void testExclusive() throws Exception {
-        runTest( TestExclusive.class.getName() );
+    public void testEncryptedDb() throws Exception {
+        runTest( TestEncryptedDb.class.getName() );
     }
     
     @Test
@@ -312,6 +363,290 @@ public class GaeH2TestCase extends GaeVfsTestCase {
     @Test
     public void testView() throws Exception {
         runTest( TestView.class.getName() );
+    }
+    
+    // jaqu
+    
+    @Test
+    public void testAliasMap() throws Exception {
+        runTest( AliasMapTest.class.getName() );
+    }
+    
+    @Test
+    public void testSamples() throws Exception {
+        runTest( SamplesTest.class.getName() );
+    }
+    
+    @Test
+    public void testUpdate() throws Exception {
+        runTest( UpdateTest.class.getName() );
+    }
+
+    // jdbc
+    
+    @Test
+    public void testBatchUpdates() throws Exception {
+        runTest( TestBatchUpdates.class.getName() );
+    }
+    
+    @Test
+    public void testCallableStatement() throws Exception {
+        runTest( TestCallableStatement.class.getName() );
+    }
+    
+    @Test
+    public void testCancel() throws Exception {
+        runTest( TestCancel.class.getName() );
+    }
+    
+    @Test
+    public void testDatabaseEventListener() throws Exception {
+        runTest( TestDatabaseEventListener.class.getName() );
+    }
+    
+    @Test
+    public void testDriver() throws Exception {
+        runTest( TestDriver.class.getName() );
+    }
+    
+    @Test
+    public void testManyJdbcObjects() throws Exception {
+        runTest( TestManyJdbcObjects.class.getName() );
+    }
+    
+    @Test
+    public void testMetaData() throws Exception {
+        runTest( TestMetaData.class.getName() );
+    }
+    
+    @Test
+    public void testNativeSQL() throws Exception {
+        runTest( TestNativeSQL.class.getName() );
+    }
+    
+    @Test
+    public void testPreparedStatement() throws Exception {
+        runTest( TestPreparedStatement.class.getName() );
+    }
+    
+    @Test
+    public void testResultSet() throws Exception {
+        runTest( TestResultSet.class.getName() );
+    }
+    
+    @Test
+    public void testStatement() throws Exception {
+        runTest( TestStatement.class.getName() );
+    }
+    
+    @Test
+    public void testTransactionIsolation() throws Exception {
+        runTest( TestTransactionIsolation.class.getName() );
+    }
+    
+    @Test
+    public void testUpdatableResultSet() throws Exception {
+        runTest( TestUpdatableResultSet.class.getName() );
+    }
+    
+    @Test
+    public void testZloty() throws Exception {
+        runTest( TestZloty.class.getName() );
+    }
+
+    // jdbcx
+    
+    @Test
+    public void testConnectionPool() throws Exception {
+        runTest( TestConnectionPool.class.getName() );
+    }
+    
+    @Test
+    public void testDataSource() throws Exception {
+        runTest( TestDataSource.class.getName() );
+    }
+    
+    @Test
+    public void testXA() throws Exception {
+        runTest( TestXA.class.getName() );
+    }
+    
+    @Test
+    public void testXASimple() throws Exception {
+        runTest( TestXASimple.class.getName() );
+    }
+
+    // server
+    
+    @Test
+    public void testNestedLoop() throws Exception {
+        runTest( TestNestedLoop.class.getName() );
+    }
+
+    // mvcc & row level locking
+    
+    @Test
+    public void testMvcc1() throws Exception {
+        runTest( TestMvcc1.class.getName() );
+    }
+    
+    @Test
+    public void testMvcc2() throws Exception {
+        runTest( TestMvcc2.class.getName() );
+    }
+    
+    @Test
+    public void testMvcc3() throws Exception {
+        runTest( TestMvcc3.class.getName() );
+    }
+    
+    @Test
+    public void testMvccMultiThreaded() throws Exception {
+        runTest( TestMvccMultiThreaded.class.getName() );
+    }
+    
+    @Test
+    public void testRowLocks() throws Exception {
+        runTest( TestRowLocks.class.getName() );
+    }
+
+    // synth
+    
+    @Test
+    public void testBtreeIndex() throws Exception {
+        runTest( TestBtreeIndex.class.getName() );
+    }
+    
+    @Test
+    public void testCrashAPI() throws Exception {
+        runTest( TestCrashAPI.class.getName() );
+    }
+    
+    @Test
+    public void testFuzzOptimizations() throws Exception {
+        runTest( TestFuzzOptimizations.class.getName() );
+    }
+    
+    @Test
+    public void testRandomSQL() throws Exception {
+        runTest( TestRandomSQL.class.getName() );
+    }
+    
+    @Test
+    public void testMultiThreaded() throws Exception {
+        runTest( TestMultiThreaded.class.getName() );
+    }
+    
+    // testUnit
+    
+    @Test
+    public void testBitField() throws Exception {
+        runTest( TestBitField.class.getName() );
+    }
+    
+    @Test
+    public void testCache() throws Exception {
+        runTest( TestCache.class.getName() );
+    }
+    
+    @Test
+    public void testClearReferences() throws Exception {
+        runTest( TestClearReferences.class.getName() );
+    }
+    
+    @Test
+    public void testCompress() throws Exception {
+        runTest( TestCompress.class.getName() );
+    }
+    
+    @Test
+    public void testDataPage() throws Exception {
+        runTest( TestDataPage.class.getName() );
+    }
+    
+    @Test
+    public void testDate() throws Exception {
+        runTest( TestDate.class.getName() );
+    }
+    
+    @Test
+    public void testDateIso8601() throws Exception {
+        runTest( TestDateIso8601.class.getName() );
+    }
+    
+    @Test
+    public void testFile() throws Exception {
+        runTest( TestFile.class.getName() );
+    }
+    
+    @Test
+    public void testIntArray() throws Exception {
+        runTest( TestIntArray.class.getName() );
+    }
+    
+    @Test
+    public void testIntIntHashMap() throws Exception {
+        runTest( TestIntIntHashMap.class.getName() );
+    }
+    
+    @Test
+    public void testMathUtils() throws Exception {
+        runTest( TestMathUtils.class.getName() );
+    }
+    
+    @Test
+    public void testOverflow() throws Exception {
+        runTest( TestOverflow.class.getName() );
+    }
+    
+    @Test
+    public void testPattern() throws Exception {
+        runTest( TestPattern.class.getName() );
+    }
+    
+    @Test
+    public void testRecovery() throws Exception {
+        runTest( TestRecovery.class.getName() );
+    }
+    
+    @Test
+    public void testScriptReader() throws Exception {
+        runTest( TestScriptReader.class.getName() );
+    }
+    
+    @Test
+    public void testSecurity() throws Exception {
+        runTest( TestSecurity.class.getName() );
+    }
+    
+    @Test
+    public void testShell() throws Exception {
+        runTest( TestShell.class.getName() );
+    }
+    
+    @Test
+    public void testStringCache() throws Exception {
+        runTest( TestStringCache.class.getName() );
+    }
+    
+    @Test
+    public void testStringUtils() throws Exception {
+        runTest( TestStringUtils.class.getName() );
+    }
+    
+    @Test
+    public void testValue() throws Exception {
+        runTest( TestValue.class.getName() );
+    }
+    
+    @Test
+    public void testValueHashMap() throws Exception {
+        runTest( TestValueHashMap.class.getName() );
+    }
+    
+    @Test
+    public void testValueMemory() throws Exception {
+        runTest( TestRandomSQL.class.getName() );
     }
     
     /**
