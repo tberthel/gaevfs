7604aa0f107237bff6ea6cdd4b7e8535aa87a5d1
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-10-23 18:25:48 +0000

    tweak

--- trunk/test/src/com/newatlanta/appengine/junit/LocalServiceTestCase.java
+++ trunk/test/src/com/newatlanta/appengine/junit/LocalServiceTestCase.java
@@ -30,15 +30,15 @@ public abstract class LocalServiceTestCase extends TestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        ApiProxy.setEnvironmentForCurrentThread(new TestEnvironment());
-        ApiProxy.setDelegate(new ApiProxyLocalImpl(new File(".")) {});
+        ApiProxy.setEnvironmentForCurrentThread( new TestEnvironment() );
+        ApiProxy.setDelegate( new ApiProxyLocalImpl( new File( "." ) ) {} );
     }
 
     @Override
     public void tearDown() throws Exception {
         // not strictly necessary to null these out but there's no harm either
-        ApiProxy.setDelegate(null);
-        ApiProxy.setEnvironmentForCurrentThread(null);
+        ApiProxy.setDelegate( null );
+        ApiProxy.clearEnvironmentForCurrentThread();
         super.tearDown();
     }
 }
