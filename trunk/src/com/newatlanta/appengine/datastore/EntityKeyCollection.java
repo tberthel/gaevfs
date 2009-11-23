4a504aeff5331c3e5c1fc029d549f4a87ec5725a
vbonfanti <vbonfanti@5fd1dbc8-5ce3-11de-ac86-572c61aa6b8a>
2009-11-23 19:26:42 +0000

    use Collection rather than Iterable (Collection implements Iterable)

--- trunk/src/com/newatlanta/appengine/datastore/EntityKeyCollection.java
+++ trunk/src/com/newatlanta/appengine/datastore/EntityKeyCollection.java
@@ -23,59 +23,55 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 
 /**
- * A utility class that wraps an Iterable<Entity> and treats it as an
- * Iterable<Key>; this is useful when doing keys-only queries to treat
- * the return value as an Iterable<Key>
+ * A utility class that wraps an Collection<Entity> and treats it as an
+ * Collection<Key>; this is useful when doing keys-only queries to treat
+ * the return value as an Collection<Key>
  * 
  * @author <a href="mailto:vbonfanti@gmail.com">Vince Bonfanti</a>
  */
 public class EntityKeyCollection extends AbstractCollection<Key> {
     
-    private Iterable<Entity> entityIterable;
+    private Collection<Entity> entities;
    
-    public static EntityKeyCollection wrap( Iterable<Entity> entityIterable ) {
-        return new EntityKeyCollection( entityIterable );
+    public static EntityKeyCollection wrap( Collection<Entity> entities ) {
+        return new EntityKeyCollection( entities );
     }
     
-    private EntityKeyCollection( Iterable<Entity> entityIterable ) {
-        this.entityIterable = entityIterable;
+    private EntityKeyCollection( Collection<Entity> entityCollection ) {
+        this.entities = entityCollection;
     }
 
     @Override
     public Iterator<Key> iterator() {
-        return new EntityKeyIterator( entityIterable.iterator() );
+        return new EntityKeyIterator( entities.iterator() );
     }
 
     @Override
     public int size() {
-        if ( entityIterable instanceof Collection<?> ) {
-            return ((Collection<?>)entityIterable).size();
-        }
-        throw new UnsupportedOperationException();
+        return entities.size();
     }
     
     private class EntityKeyIterator implements Iterator<Key> {
         
-        private Iterator<Entity> entityIterator;
+        private Iterator<Entity> entityIter;
         
-        private EntityKeyIterator( Iterator<Entity> entityIterator ) {
-            this.entityIterator = entityIterator;
+        private EntityKeyIterator( Iterator<Entity> entityIter ) {
+            this.entityIter = entityIter;
         }
 
         @Override
         public boolean hasNext() {
-            return entityIterator.hasNext();
+            return entityIter.hasNext();
         }
 
         @Override
         public Key next() {
-             Entity entity = entityIterator.next();
-             return entity.getKey();
+             return entityIter.next().getKey();
         }
 
         @Override
         public void remove() {
-            entityIterator.remove();
+            entityIter.remove();
         }
     }
 }
